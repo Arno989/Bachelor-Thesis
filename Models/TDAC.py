9dcc2e3ac604b41543567c4343de553779b62f07
Arno Defauw <arno@delandtsheer.eu>
2021-01-08 14:44:43 +0100

    renamed AC agent to TDAC and removed A2C

--- Models/TDAC.py
+++ Models/TDAC.py
@@ -9,33 +9,35 @@ from tensorflow.keras.layers import Dense, Input, Conv1D, Dropout
 from tensorflow.keras.optimizers import Adam
 
 
-class AC:
-    def __init__(self, alpha, beta, action_space, state_space, gamma=0.99, layer1_size=1024, layer2_size=512):
+class TDAC:
+    def __init__(self, alpha, beta, action_space, state_space, gamma=0.99):
         self.gamma = gamma
         self.alpha = alpha
         self.beta = beta
-        self.input_dims = state_space
-        self.fc1_dims = layer1_size
-        self.fc2_dims = layer2_size
+        self.state_space = state_space
         self.n_actions = action_space
 
         self.actor, self.critic, self.policy = self.build_actor_critic_network()
         self.action_space = [i for i in range(self.n_actions)]
 
     def build_actor_critic_network(self):
-        NN_input = Input(shape=(self.input_dims[0],))
+        # shared base network
+        NN_input = Input(shape=(self.state_space[0],))
         delta = Input(shape=[1])
-        dense1 = Dense(self.fc1_dims, activation='relu')(NN_input)
-        dense2 = Dense(self.fc2_dims, activation='relu')(dense1)
+        dense1 = Dense(30, activation='relu')(NN_input)
+        dense2 = Dense(30, activation='relu')(dense1)
+        # final split layers
         probs = Dense(self.n_actions, activation='softmax')(dense2)
         values = Dense(1, activation='linear')(dense2)
 
+        # actor loss function
         def custom_loss(y_true, y_pred):
             out = K.clip(y_pred, 1e-8, 1-1e-8)
             log_lik = y_true*K.log(out)
 
             return K.sum(-log_lik*delta)
 
+        # compose networks
         actor = Model(inputs=[NN_input, delta], outputs=[probs])
         actor.compile(optimizer=Adam(lr=self.alpha), loss=custom_loss)
 
@@ -47,23 +49,29 @@ class AC:
         return actor, critic, policy
 
     def choose_action(self, state):
+        # reshape for prediction
         state = state.reshape([1, state.shape[0]])
+        # make probability distribution with predict 
         probabilities = self.policy.predict(state)[0]
-        action = np.random.choice(self.action_space, p=probabilities)
+        # return acion based on probabilities
+        return np.random.choice(self.action_space, p=probabilities)
 
-        return action
-
-    def learn(self, state, action, reward, state_, done):
-        state = state[np.newaxis,:]
-        state_ = state_[np.newaxis,:]
-        critic_value_ = self.critic.predict(state_)
+    def learn(self, state, action, reward, next_state, done):
+        # reshape for prediction
+        state = state.reshape([1, state.shape[0]])
+        next_state = next_state.reshape([1, next_state.shape[0]])
+        # predict critic values
         critic_value = self.critic.predict(state)
+        critic_value_ = self.critic.predict(next_state)
 
-        target = reward + self.gamma*critic_value_*(1-int(done))
+        # calculate correction for action-value
+        target = reward + self.gamma * critic_value_ * (1 - int(done))
         delta =  target - critic_value
 
+        # no clue
         actions = np.zeros([1, self.n_actions])
         actions[np.arange(1), action] = 1
 
+        #update networks
         self.actor.fit([state, delta], actions, verbose=0)
         self.critic.fit(state, target, verbose=0) 
